acde70c257a6adde481ee84536937490f3085223
Konrad Pawlowski <konrad@Konrads-Air.lan>
2026-02-05 02:50:51 +0100

    Base Page and Login Page refactor

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,18 +1,23 @@
-class LoginPage:
+from pages.base_page import BasePage
+
+class LoginPage(BasePage):
     def __init__(self, page):
-        self.page = page
+        super().__init__(page) #initialize constructor of a parent class
+        self.base_url = "https://www.saucedemo.com/"
+        
+        # selectors
         self.username_input = "#user-name"
         self.password_input = "#password"
         self.login_button = "#login-button"
         self.error_message = "[data-test='error']"
 
-    def navigate(self):
-        self.page.goto("https://www.saucedemo.com/")
+    def open(self):
+        self.navigate(self.base_url)
 
     def login(self, username, password):
-        self.page.fill(self.username_input, username)
-        self.page.fill(self.password_input, password)
-        self.page.click(self.login_button)
+        self.fill(self.username_input, username)
+        self.fill(self.password_input, password)
+        self.click(self.login_button)
 
     def get_error_message(self):
-        return self.page.inner_text(self.error_message)
+        return self.get_text(self.error_message)
